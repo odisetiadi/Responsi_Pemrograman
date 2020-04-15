31d3c133322124d5b8deea1c19f56a23bd6e85bb
Dwi Putra Setiadi <61934529+odisetiadi@users.noreply.github.com>
2020-04-15 15:31:44 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ResponsiPemrograman2917
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Karyawan karyawan1 = new Karyawan("190302123", "paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan("190302124", "jono", 2000000);
+            Console.WriteLine("No Nik/nama   Gaji Bulanan");
+            Console.WriteLine("1. " + karyawan1.NIK + " "    + karyawan1.NAMA + "\t" + karyawan1.gajibulanan);
+            Console.WriteLine("1. " + karyawan2.NIK + " "  + karyawan2.NAMA + "\t\t" + karyawan2.gajibulanan);
+            Console.WriteLine("\n\nAsyiiiiik Ada Kenaikan Gaji 10%!!\n");
+            Console.WriteLine("No Nik/nama        Gaji Bulanan")
+            n1 = (10 * karyawan1.gajibulanan) / 100;
+            n2 = (10 * karyawan2.gajibulanan) / 100;
+            Console.WriteLine("1. " + karyawan1.NIK + " " + karyawan1.NAMA + "\t" + (n1 + karyawan1.gajibulanan));
+            Console.WriteLine("1. " + karyawan2.NIK + " " + karyawan2.NAMA+ "\t\t" + (n2 + karyawan2.gajibulanan));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman2917
+{
+    class Karyawan
+    {
+
+        public string NIK;
+        public string NAMA;
+        public int gajibulanan;
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+            this.NIK = NIK;
+            this.NAMA = NAMA;
+            if (gajibulanan < 0)
+            {
+                this.NIK = "0";
+                Console.WriteLine("Maaf Gaji Yang Anda Masukkan Tidak boleh Kurang Dari 0");
+            }
+            else
+            {
+                this.gajibulanan = gajibulanan;
+            }
+        }
+    }
+}
